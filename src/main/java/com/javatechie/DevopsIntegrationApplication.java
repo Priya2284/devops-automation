6e9b176ac0c831d5b2829159b8ea8970db7b131f
aamir490 <112395400+aamir490@users.noreply.github.com>
2023-06-28 21:55:03 +0530

    Update DevopsIntegrationApplication.java
    wq

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hi Friends Latest Devops Projects Uploaded Daily Based . LiKe & Share this video & also Subscribe";
+		return "Hi Friend : my name is aamir";
 	}
 
 	public static void main(String[] args) {
